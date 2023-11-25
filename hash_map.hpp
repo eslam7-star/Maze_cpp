2dc1e3c6829856834d8e812cba397475f3aad0af
0xEslam <eslam.elemam@gmail.com>
2023-11-25 19:30:13 +0200

    0x2

--- hash_map.hpp
+++ hash_map.hpp
@@ -17,7 +17,6 @@ public:
 Hash_map(int size);
 int hash_function(const cell& c);
 void insert(const cell& c , int index); 
-void delete_cell(int index);
 bool isfound(const cell& c);
 ~Hash_map();
 
