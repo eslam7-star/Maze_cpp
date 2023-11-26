cd60ae676f6b1aa1845cb98e3f7bc89976478dfd
yousefzaki9993 <yousefzaki9993@gamil.com>
2023-11-26 14:05:10 +0200

    zaki commit

--- Cell.cpp
+++ Cell.cpp
@@ -24,8 +24,13 @@ class cell{
     }
 
     bool issolid(){
-        return issolid;
+        return iswall;
     }
+     bool operator == (cell c){
+        if(x==c.get_x()&&y==c.get_Y()&&iswall==c.issolid()) {
+          return true;
+        }
+        return false;}
 
 
 }r(1,0),l(-1,0),n(0,1),s(0,-1);     // directions
