16e5a721664dbb530518b357c29c5c5758feac1e
adelmohamed11 <129177338+adelmohamed11@users.noreply.github.com>
2023-11-26 11:30:17 +0200

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -5,3 +5,12 @@ Stack::Stack(){
     myTop = 0;
 }
 
+StackElement Stack::top() const{
+    if(!empty())
+        return myTop->direction;
+    
+    else{
+        cerr<<"***Error! Stack is empty***";
+        return;
+    }
+}
