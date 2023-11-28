f739d232b3d783b9cb34e2b7ea9f0772a841b6d9
yousefzaki9993 <yousefzaki9993@gmail.com>
2023-11-28 09:15:04 +0200

    zaki commit

--- stack.cpp
+++ stack.cpp
@@ -50,3 +50,15 @@ bool Stack::empty() const{
     return myTop==NULL;
 }
 
+void Stack::push(const StackElement& value){
+    if (empty())
+    {
+      myTop =new Stack::Node(NULL); 
+      myTop->direction=value;
+    }
+    else{ 
+        myTop->next=new Stack::Node(NULL);
+        myTop = myTop->next;
+        myTop->direction=value;
+    }
+}
